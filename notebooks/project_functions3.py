1520541f10f17cf9fbd729b3795361fd8ead276b
jyo <jyotigrayatrisahoo@gmail.com>
2022-11-07 16:58:47 -0800

    task 3 completed

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -1,12 +1,17 @@
+import pandas as pd;
+
 def load_and_process(url_or_path):
     df = (
-        pd.read_csv(url_or_path)
-        .rename(columns={'Unnamed: 0':'student_id', 'failures.x':'failures', 'activities.x':'activities', 'romantic.x':'romantic_relationship','Walc.x':'Weekend_alcohol','Dalc.x':'Weekday_alcohol', 'famrel.x':'family_relationship','freetime.x':'freetime','goout.x':'socialising','absences.x':'absence'})
-    )
+        pd.read_csv(url_or_path))
+        # .rename(columns={'Unnamed: 0':'student_ic', 'failures.x':'failures', 'activities.x':'activities', 'romantic.x':'romantic_relationship','Walc.x':'Weekend_alcohol','Dalc.x':'Weekday_alcohol', 'famrel.x':'family_relationship','freetime.x':'freetime','goout.x':'socialising','absences.x':'absence'})    )
     df2 = (
-        df.assign(math_avg = lambda x :df['G1.x']+df['G2.x']+df['G3.x'])
+        df
+        .assign(math_avg = lambda x :df['G1.x']+df['G2.x']+df['G3.x'])
         .assign(por_avg = lambda x :df['G1.y']+df['G2.y']+df['G3.y'])
-        .drop(['G1.x','G2.x','G3.x','G1.y','G2.y','G3.y'],axis=1)
+    )
+    
+    df3 = (
+        df2.drop(['sex','school','address','Medu','Fedu','Mjob','Fjob','reason','guardian.x','guardian.y','traveltime.x','traveltime.y','schoolsup.x','schoolsup.y','paid.x','paid.y','famsup.x','famsup.y','nursery','health.x','health.y','higher.x','higher.y','Dalc.y','Walc.y','famrel.y','freetime.y','absences.y','studytime.y','romantic.y','goout.y','failures.y','activities.y','studytime.x'],axis=1)
     )
 
-    return df2
+    return df3
